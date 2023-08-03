e81534327e77ef403b79b3264153fa8b99267be0
Ale Gomez <gom20347@uvg.edu.gt>
2023-08-03 02:36:49 -0600

    added hamming

--- Fletcher_Checksum/emisor.js
+++ Fletcher_Checksum/emisor.js
@@ -1,21 +1,27 @@
 function fletcherChecksumEmisor(message) {
-    // Paso 2: Calcular la información adicional (Fletcher checksum)
-    let sum1 = 0;
-    let sum2 = 0;
-    for (let i = 0; i < message.length; i++) {
-      sum1 = (sum1 + parseInt(message[i], 2)) % 255;
-      sum2 = (sum2 + sum1) % 255;
-    }
-  
-    // Paso 3: Devolver el mensaje en binario concatenado con la información adicional
-    const checksum = (sum1.toString(2).padStart(8, '0') + sum2.toString(2).padStart(8, '0'));
-    return message + checksum;
+  // Paso 2: Calcular la información adicional (Fletcher checksum)
+  let sum1 = 0
+  let sum2 = 0
+  for (let i = 0; i < message.length; i++) {
+    sum1 = (sum1 + parseInt(message[i], 2)) % 255
+    sum2 = (sum2 + sum1) % 255
   }
-  
-  // Ejemplo de uso:
-  const tramas = ["110101100000101000101001", "11010110000001001", "100000101000101001", "11010101001"];
-  for (var i = 0; i < tramas.length; i++) {
-    const tramaConChecksum = fletcherChecksumEmisor(tramas[i]);
-    console.log("Trama enviada:", tramaConChecksum);
-  }
-  
+
+  // Paso 3: Devolver el mensaje en binario concatenado con la información adicional
+  const checksum =
+    sum1.toString(2).padStart(8, '0') + sum2.toString(2).padStart(8, '0')
+  return message + checksum
+}
+
+// Ejemplo de uso:
+const tramas = [
+  '110101100000101000101001',
+  '11010110000001001',
+  '100000101000101001',
+  '11010101001',
+]
+
+for (var i = 0; i < tramas.length; i++) {
+  const tramaConChecksum = fletcherChecksumEmisor(tramas[i])
+  console.log('Trama enviada:', tramaConChecksum)
+}
--- Fletcher_Checksum/receptor.py
+++ Fletcher_Checksum/receptor.py
@@ -10,7 +10,7 @@ def fletcher_checksum_receptor(received_message):
         sum2 = (sum2 + sum1) % 255
 
     # Paso 4: Comparar el checksum calculado con el recibido
-    calculated_checksum = format(sum1, '08b') + format(sum2, '08b')
+    calculated_checksum = format(sum1, "08b") + format(sum2, "08b")
     if calculated_checksum == received_checksum:
         # a. No se detectaron errores
         print("Mensaje recibido sin errores:", message)
@@ -20,7 +20,11 @@ def fletcher_checksum_receptor(received_message):
             # c. Se detectaron y corrigieron errores
             error_bit = int(received_checksum[8:], 2)
             if error_bit < len(message):
-                corrected_message = message[:error_bit] + str(1 - int(message[error_bit], 2)) + message[error_bit + 1:]
+                corrected_message = (
+                    message[:error_bit]
+                    + str(1 - int(message[error_bit], 2))
+                    + message[error_bit + 1 :]
+                )
                 print("Se corrigieron errores. Bit corregido en posición:", error_bit)
                 print("Trama corregida:", corrected_message)
             else:
@@ -28,11 +32,22 @@ def fletcher_checksum_receptor(received_message):
         else:
             print("La trama se descarta por detectar errores.")
 
+
 print("Tramas correctas")
-tramas_correctas = ["1101011000001010001010010000101010001010", "110101100000010010000011101001011", "1000001010001010010000011000110011", "110101010010000011000101000"]
+tramas_correctas = [
+    "1101011000001010001010010000101010001010",
+    "110101100000010010000011101001011",
+    "1000001010001010010000011000110011",
+    "110101010010000011000101000",
+]
 for trama in tramas_correctas:
     fletcher_checksum_receptor(trama)
 print("Tramas modificadas")
-tramas_errores = ["1101011010001010001010010000101010011010", "110101100000010010000011101001010", "1000011010001010010000011000110011", "110101010110000011000101000"]
+tramas_errores = [
+    "1101011010001010001010010000101010011010",
+    "110101100000010010000011101001010",
+    "1000011010001010010000011000110011",
+    "110101010110000011000101000",
+]
 for trama in tramas_errores:
     fletcher_checksum_receptor(trama)

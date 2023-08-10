71003c1667e7d500e753fe0b3423fe2d115a58e4
Juan Carlos Bajan <baj20109@uvg.edu.gt>
2023-08-10 11:12:02 -0600

    Parte 2

--- Fletcher_Checksum/emisor.js
+++ Fletcher_Checksum/emisor.js
@@ -1,5 +1,7 @@
+const fs = require('fs');
+const net = require('net');
+
 function fletcherChecksumEmisor(message) {
-  // Paso 2: Calcular la información adicional (Fletcher checksum)
   let sum1 = 0
   let sum2 = 0
   for (let i = 0; i < message.length; i++) {
@@ -7,21 +9,70 @@ function fletcherChecksumEmisor(message) {
     sum2 = (sum2 + sum1) % 255
   }
 
-  // Paso 3: Devolver el mensaje en binario concatenado con la información adicional
   const checksum =
     sum1.toString(2).padStart(8, '0') + sum2.toString(2).padStart(8, '0')
   return message + checksum
 }
 
-// Ejemplo de uso:
-const tramas = [
-  '110101100000101000101001',
-  '11010110000001001',
-  '100000101000101001',
-  '11010101001',
-]
-
-for (var i = 0; i < tramas.length; i++) {
-  const tramaConChecksum = fletcherChecksumEmisor(tramas[i])
-  console.log('Trama enviada:', tramaConChecksum)
+function process(message) {
+  const binario = message.split('').map(char => {
+    let bits = char.charCodeAt(0).toString(2).padStart(8, '0');
+    return Array.from(bits).map(bit => {
+      if (Math.random() < 0.01) {
+        return bit === '0' ? '1' : '0';
+      }
+      return bit;
+    }).join('');
+  }).join('');
+  return binario
 }
+
+const client = new net.Socket();
+
+client.on('data', data => {
+  console.log('Recibido:', data.toString());
+});
+
+client.on('close', () => {
+  console.log('Conexión cerrada');
+});
+
+client.on('error', err => {
+  console.error('Ocurrió un error:', err);
+});
+
+client.connect(1050, '127.0.0.1', () => {
+  console.log('Conectado al puerto 1050');
+
+
+  fs.readFile('palabras.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Ocurrió un error al leer el archivo:', err);
+      return;
+    }
+    
+    const lineas = data.split('\n');
+    
+    function sendTrama(index) {
+      if (index >= lineas.length * 440) {
+        // Se completaron las repeticiones necesarias.
+        // Puedes hacer algo aquí, como cerrar la conexión si es necesario.
+        client.end()
+        return;
+      }
+  
+      const palabraIndex = index % lineas.length;
+      const trama = process(lineas[palabraIndex]);
+      const fletcher_trama = fletcherChecksumEmisor(trama);
+      const buffer = Buffer.from(fletcher_trama, 'binary');
+      client.write(buffer);
+      console.log('Trama enviada: ' + fletcher_trama);
+  
+      setTimeout(() => {
+        sendTrama(index + 1); // Envío de la siguiente trama después de 1 segundo.
+      }, 25);
+    }
+  
+    sendTrama(0); // Iniciar el envío de tramas.
+  });
+});
--- Fletcher_Checksum/receptor.py
+++ Fletcher_Checksum/receptor.py
@@ -1,3 +1,15 @@
+import socket
+import difflib
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def binary_to_ascii(binary_str):
+    ascii_str = ""
+    for i in range(0, len(binary_str), 8):
+        byte = binary_str[i:i+8]
+        ascii_str += chr(int(byte, 2))
+    return ascii_str
+
 def fletcher_checksum_receptor(received_message):
     # Paso 2: Obtener el mensaje recibido y el checksum adjunto
     message = received_message[:-16]
@@ -13,7 +25,7 @@ def fletcher_checksum_receptor(received_message):
     calculated_checksum = format(sum1, "08b") + format(sum2, "08b")
     if calculated_checksum == received_checksum:
         # a. No se detectaron errores
-        print("Mensaje recibido sin errores:", message)
+        return binary_to_ascii(message)
     else:
         # b. Se detectaron errores
         if sum1 == int(received_checksum[:8], 2):
@@ -25,29 +37,59 @@ def fletcher_checksum_receptor(received_message):
                     + str(1 - int(message[error_bit], 2))
                     + message[error_bit + 1 :]
                 )
-                print("Se corrigieron errores. Bit corregido en posición:", error_bit)
-                print("Trama corregida:", corrected_message)
+                return binary_to_ascii(corrected_message)
             else:
-                print("La trama se descarta por detectar errores.")
+                return None
         else:
-            print("La trama se descarta por detectar errores.")
+            return None
+
+def calcular_similitud(palabra1, palabra2):
+    similarity = difflib.SequenceMatcher(None, palabra1, palabra2).ratio()
+    similarity_percentage = similarity * 100
+    return similarity_percentage
+
+# Crear un socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Vincular el socket al puerto 8000
+server_socket.bind(('localhost', 1050))
+
+# Escuchar conexiones entrantes con una cola de tamaño 1
+server_socket.listen(1)
+print('Esperando conexiones en el puerto 1050...')
+
+# Aceptar una conexión
+client_socket, address = server_socket.accept()
+print('Conexión desde:', address)
+
+archivo = "palabras.txt"  # Cambia esto al nombre de tu archivo
+lineas = []
+
+# Abre el archivo en modo lectura
+with open(archivo, "r") as f:
+    for linea in f:
+        lineas.append(linea.strip())
+
+message_index = 0
+similitudes =[]
+
+# Leer datos en un bucle y cerrar cuando esté hecho
+while True:
+    data = client_socket.recv(1024)
+    if data.decode() == '':
+        with open('similitudes.txt', 'w') as file:
+            for similitud in similitudes:
+                file.write(str(similitud) + '\n')
+        break
+    word = fletcher_checksum_receptor(data.decode())
+    if word is not None and word != '':
+        linea_word = lineas[message_index % len(lineas)]
+        similitud = round(calcular_similitud(word, linea_word))
+        similitudes.append(similitud)
+        print(message_index, word,linea_word, similitud)
+        message_index += 1
+    else:
+        similitudes.append(0)
+        message_index += 1
 
 
-print("Tramas correctas")
-tramas_correctas = [
-    "1101011000001010001010010000101010001010",
-    "110101100000010010000011101001011",
-    "1000001010001010010000011000110011",
-    "110101010010000011000101000",
-]
-for trama in tramas_correctas:
-    fletcher_checksum_receptor(trama)
-print("Tramas modificadas")
-tramas_errores = [
-    "1101011010001010001010010000101010011010",
-    "110101100000010010000011101001010",
-    "1000011010001010010000011000110011",
-    "110101010110000011000101000",
-]
-for trama in tramas_errores:
-    fletcher_checksum_receptor(trama)
